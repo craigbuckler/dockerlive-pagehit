5fa9502f338c2616592077361443021722b14523
Craig Buckler <craig@optimalworks.net>
2021-03-03 17:33:49 +0000

    added referrerpolicy="unsafe-url" to <img>

--- testsite/template/htmlfoot.php
+++ testsite/template/htmlfoot.php
@@ -1,6 +1,6 @@
 <footer>
 
-  <p>This page has been viewed <img src="http://localhost:8104/hit.svg" /> times.</p>
+  <p>This page has been viewed <img src="http://localhost:8104/hit.svg" referrerpolicy="unsafe-url" /> times.</p>
   <p>Running on: <?php echo $_SERVER['SERVER_NAME']; ?></p>
 
 </footer>
